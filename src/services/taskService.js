295644d44bfc83c7a1d18e05d2dcb56097d08e19
Johan Svensson <loool65@hotmail.com>
2026-03-26 12:19:36 +0100

    Implement taskService with full API calls for todo CRUD and filters

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,1 +1,48 @@
-//*todo: implement taskService and call the API
+import apiClient from './apiClient';
+
+export const taskService = {
+    getAll: async () => {
+        const response = await apiClient.get('/todo');
+        return response.data;
+    },
+
+    getById: async (id) => {
+        const response = await apiClient.get(`/todo/${id}`);
+        return response.data;
+    },
+
+    getByPerson: async (personId) => {
+        const response = await apiClient.get(`/todo/person/${personId}`);
+        return response.data;
+    },
+
+    getByStatus: async (completed) => {
+        const response = await apiClient.get('/todo/status', { params: { completed } });
+        return response.data;
+    },
+
+    getOverdue: async () => {
+        const response = await apiClient.get('/todo/overdue');
+        return response.data;
+    },
+
+    create: async (todoDto, files = []) => {
+        const formData = new FormData();
+        formData.append('todo', new Blob([JSON.stringify(todoDto)], { type: 'application/json' }));
+        files.forEach(file => formData.append('files', file));
+        const response = await apiClient.post('/todo', formData);
+        return response.data;
+    },
+
+    update: async (id, todoDto, files = []) => {
+        const formData = new FormData();
+        formData.append('todo', new Blob([JSON.stringify(todoDto)], { type: 'application/json' }));
+        files.forEach(file => formData.append('files', file));
+        const response = await apiClient.put(`/todo/${id}`, formData);
+        return response.data;
+    },
+
+    delete: async (id) => {
+        await apiClient.delete(`/todo/${id}`);
+    },
+};
